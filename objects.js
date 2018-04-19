6ad54d7e9a9194a76b64d37082c2673700247a6d
Devin Cohen <devin.cohen77@gmail.com>
2018-04-19 00:16:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
 playlist['My Bloody Valentine'] = 'Sometimes'
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({ [artistName]: 'songTitle' });
+  return Object.assign(playlist, { [artistName]: 'songTitle' });
 }
 
 updatePlaylist( playlist, "Phil Ochs", "Here's to the State of Mississippi")
