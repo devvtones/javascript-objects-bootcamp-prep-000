c61b31eaacb375651f22d1df59c071ff9d83fffd
Devin Cohen <devin.cohen77@gmail.com>
2018-04-19 00:09:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,3 +9,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 updatePlaylist( playlist, "Phil Ochs", "Here's to the State of Mississippi")
+
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.slowdive;
+  return playlist;
+}
